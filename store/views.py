9e1c4978e89f7d1ad72cf64c02fc78aec1594815
Vikas <vp2841144@gmail.com>
2023-02-01 00:59:56 +0530

    5 commit

--- store/views.py
+++ store/views.py
@@ -28,7 +28,6 @@ def product_detail(request,category_slug,product_slug):
     try:
         single_product = Product.objects.get(category__slug = category_slug, slug = product_slug)
         in_cart = CartItem.objects.filter(cart__cart_id=_cart_id(request),product=single_product).exists()
-        return
     except Exception as e:
         raise e 
         
