ddf505eb5ee9e48603a3ef5df565edc3b0784885
Vikas <vp2841144@gmail.com>
2023-02-12 12:24:23 +0530

    Mail Correction

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -145,7 +145,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_HOST= 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMIAL_HOST_USER = 'techlifes18@gmail.com'
+EMAIL_HOST_USER = 'techlifes18@gmail.com'
 EMAIL_HOST_PASSWORD = "jermhibdjpywnysk"
 EMAIL_USE_TLS = True
 
